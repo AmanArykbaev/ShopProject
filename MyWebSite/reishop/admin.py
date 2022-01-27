06425d9e9c3ad406c8e12b0dcac178d23b0a222d
AmanArykbaev <aman.arykbaev@gmail.com>
2022-01-26 22:51:06 -0500

    Added About and contact us html codes

--- MyWebSite/reishop/admin.py
+++ MyWebSite/reishop/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
-from .models import Clothes
+from .models import *
 
 
 class ShopAdmin(admin.ModelAdmin):
-    list_display = ('id', 'cloth_name', 'cloth_brand', 'cloth_size', 'cloth_date_release')
+    list_display = ('id', 'cloth_name', 'cloth_brand', 'cloth_size', 'cloth_date_release', 'category', 'is_published')
     list_display_links = ('id', 'cloth_name', 'cloth_brand')
-    search_fields = ('cloth_name', 'cloth_brand', 'cloth_size', 'cloth_date_release')
+    search_fields = ('cloth_name', 'cloth_brand', 'cloth_size', 'cloth_date_release', 'category')
+    list_filter = ('cloth_date_release', 'is_published', 'category')
+    list_editable = ('is_published',)
+
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'cloth_name')
+    list_display_links = ('id', 'cloth_name')
+    search_fields = ('cloth_name',)
 
 
 admin.site.register(Clothes, ShopAdmin)
+admin.site.register(Category, CategoryAdmin)
--- MyWebSite/reishop/models.py
+++ MyWebSite/reishop/models.py
@@ -8,7 +8,9 @@ class Clothes(models.Model):
     cloth_brand = models.CharField(max_length=200, verbose_name='Brand Name')
     cloth_size = models.CharField(max_length=200, verbose_name='Size')
     cloth_date_release = models.DateTimeField(auto_now_add=True, verbose_name='Date')
-    # photo = models.ImageField(upload_to='photos/%Y/%m/%d')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d', default='')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Category')
+    is_published = models.BooleanField(default=True, verbose_name='Published')
 
     def __str__(self):
         return self.cloth_name
@@ -17,3 +19,15 @@ class Clothes(models.Model):
         verbose_name = 'Clothes'
         verbose_name_plural = 'Clothes'
         ordering = ['cloth_date_release']
+
+
+class Category(models.Model):
+    cloth_name = models.CharField(max_length=150, db_index=True, verbose_name="Category")
+
+    def __str__(self):
+        return self.cloth_name
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        ordering = ['cloth_name']
