63ab05ea90efc0ad1657d28b3c57821e5224c77a
AmanArykbaev <aman.arykbaev@gmail.com>
2022-02-08 23:00:46 -0500

    Changed to List View classes

--- MyWebSite/reishop/admin.py
+++ MyWebSite/reishop/admin.py
@@ -3,7 +3,7 @@ from .models import *
 
 
 class ShopAdmin(admin.ModelAdmin):
-    list_display = ('id', 'cloth_name', 'cloth_brand', 'cloth_size', 'cloth_date_release', 'category', 'is_published')
+    list_display = ('id', 'cloth_name', 'cloth_brand', 'cloth_size', 'cloth_date_release', 'category', 'is_published', 'photo')
     list_display_links = ('id', 'cloth_name', 'cloth_brand')
     search_fields = ('cloth_name', 'cloth_brand', 'cloth_size', 'cloth_date_release', 'category')
     list_filter = ('cloth_date_release', 'is_published', 'category')
